e18cbbc685ae9ea4158ba88efcb6e9094af60d0c
David Stanley <davearthur.stanley@gmail.com>
2018-03-21 19:57:28 -0400

    Added code for controlling dry run to script

--- script_trim_foldernames.m
+++ script_trim_foldernames.m
@@ -3,20 +3,21 @@
 %% Rename hcurrent6 folders
 
 do_git_mv = true;
-func_trim_foldernames('d180207_t032136__hcurrent6',do_git_mv);
+dry_run = true;
+func_trim_foldernames('d180207_t032136__hcurrent6',do_git_mv,dry_run);
 
 %% Rename IB PPStim folders
 
-func_trim_foldernames('d180216_t231011__IBPPStim1',do_git_mv);
-func_trim_foldernames('d180217_t170004__IBPPStim2',do_git_mv);
-func_trim_foldernames('d180217_t180619__IBPPStim3',do_git_mv);
-func_trim_foldernames('d180217_t223539__IBPPStim4',do_git_mv);
-func_trim_foldernames('d180217_t232032__IBPPStim5',do_git_mv);
-func_trim_foldernames('d180218_t000748__IBPPStim6',do_git_mv);
-func_trim_foldernames('d180218_t004620__IBPPStim7',do_git_mv);
+func_trim_foldernames('d180216_t231011__IBPPStim1',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t170004__IBPPStim2',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t180619__IBPPStim3',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t223539__IBPPStim4',do_git_mv,dry_run);
+func_trim_foldernames('d180217_t232032__IBPPStim5',do_git_mv,dry_run);
+func_trim_foldernames('d180218_t000748__IBPPStim6',do_git_mv,dry_run);
+func_trim_foldernames('d180218_t004620__IBPPStim7',do_git_mv,dry_run);
 
 %% Rename next hcurrent6 folder...
-func_trim_foldernames('d180321_t155456__hcurrent6_default',do_git_mv);
+func_trim_foldernames('d180321_t155456__hcurrent6_default',do_git_mv,dry_run);
 
 
 
